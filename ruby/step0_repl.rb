6fca2ec21f38ffea0be8e7d073c26e7dc0c59f28
AndrewSinclair <andrew.george.sinclair@gmail.com>
2016-08-02 18:55:33 -0500

    starting fresh with ruby

--- ruby/step0_repl.rb
+++ ruby/step0_repl.rb
@@ -1,26 +1,3 @@
-require_relative "mal_readline"
+#!/usr/bin/ruby
 
-# read
-def READ(str)
-    return str
-end
-
-# eval
-def EVAL(ast, env)
-    return ast
-end
-
-# print
-def PRINT(exp)
-    return exp
-end
-
-# repl
-def REP(str)
-    return PRINT(EVAL(READ(str), {}))
-end
-
-# repl loop
-while line = _readline("user> ")
-    puts REP(line)
-end
+def READ
