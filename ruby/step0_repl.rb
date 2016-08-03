401924ff768f83d21f4e05d03c7ed15e00ada650
AndrewSinclair <andrew.george.sinclair@gmail.com>
2016-08-02 19:30:23 -0500

    Finished step 0

--- ruby/step0_repl.rb
+++ ruby/step0_repl.rb
@@ -1,3 +1,31 @@
 #!/usr/bin/ruby
 
-def READ
+def READ(param)
+  return param
+end
+
+def EVAL(param)
+  return param
+end
+
+def PRINT(param)
+  return param
+end
+
+def rep(param)
+  param = READ(param)
+  param = EVAL(param)
+  param = PRINT(param)
+end
+
+begin
+  print "user> "
+  line = gets
+
+  if line then
+    output = rep line
+    puts output
+  end
+end while line
+print "\n"
+
